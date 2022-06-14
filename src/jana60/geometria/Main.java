484567d70f592b0f7705d1803befe78e428cc924
Sowwtky <miche46ale@gmail.com>
2022-06-14 14:41:43 +0200

    esercizio completo

--- src/jana60/geometria/Main.java
+++ src/jana60/geometria/Main.java
@@ -1,10 +1,28 @@
 package jana60.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//chiamo la classe scanner e rettangolo
+		Scanner scan = new Scanner(System.in);
+		Rettangolo ret = new Rettangolo();
+		
+		//chiedo i valori all'utente
+		System.out.print("Inserire l'altezza in centimetri: ");
+		ret.altezza = scan.nextInt();
+		System.out.print("Inserire la base in centimetri: ");
+		ret.base = scan.nextInt();
+		
+		//stampo a schermo area e perimetro calcolati in base ai valori inseriti
+		System.out.println("Per il rettangolo di altezza " + ret.altezza + " cm e base " + ret.base + " cm");
+		System.out.println("L'area é: " + ret.area() + " cm");
+		System.out.println("Il perimetro é: " + ret.perimetro() + " cm");
+		
+		//chiudo scanner
+		scan.close();
 	}
 
 }
--- src/jana60/geometria/Rettangolo.java
+++ src/jana60/geometria/Rettangolo.java
@@ -1,5 +1,19 @@
 package jana60.geometria;
 
 public class Rettangolo {
-
+	//attributi base e altezza
+	int base;
+	int altezza;
+	
+	//metodo per calcolare l'area
+	int area() {
+		int calcArea = base * altezza;
+		return calcArea; 
+	}
+	
+	//metodo per calcolare il perimetro
+	int perimetro() {
+		int calcPerimetro = 2 * (base + altezza);
+		return calcPerimetro;
+	}
 }
